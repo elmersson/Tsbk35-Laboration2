c6cd2d72fa7c0ec23e210f1184f2c95d9571f91d
elmersson <elmerssonrasmus@gmail.com>
2020-02-03 15:14:15 +0100

    Skeleton code

--- Laboration2.m
+++ Laboration2.m
@@ -1,5 +1,33 @@
-clear;
-clc;
-tic;
+clear; clc; tic;
+%% Läser in filen/signalen
+[y, fs] = audioread('heyhey.wav');
+
+highFreq = max(y);
+lowFreq = min(y);
+blockSize = 1024;
+
+steps = (highFreq - lowFreq)/24;
+
+trans = mdct(y, blockSize);
+
+quant = round(trans/steps);
+
+[C,IA,IC] = unique(quant);
+
+accu = accumarray(IC(:),1);
+ 
+prob = accu/sum(sum(accu));
+
+huff = huffman(prob);
+
+yComp = imdct(quant);
+
+audiowrite('karl.wav', yComp, fs);
+
+diff = y - yComp;
+
+d = mean(diff.^2);
+
+Snrdb = 10*log10(var(y)/d);
 
 toc;
